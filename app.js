cbe3f57a08d145e961d104216d4c02d2e40c05c7
lipskij <emil.lipskij@gmail.com>
2020-08-09 15:04:03 +0300

    reduce method

--- app.js
+++ app.js
@@ -49,6 +49,15 @@ function showMillionaires() {
   updateDOM();
 }
 
+// calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 // Add new obj data arr
 function addData(obj) {
   data.push(obj);
@@ -79,3 +88,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
